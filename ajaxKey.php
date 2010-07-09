80c748100d4bdef1ba686e3fc6d7530ed237b364
SHAOHUANG PIN <shaohuang@SHAOHUANG-PINs-iMac.local>
2010-07-08 22:47:45 -0700

    coding styles

--- ajaxKey.php
+++ ajaxKey.php
@@ -10,23 +10,17 @@ header("Content-type: text/plain");
 // this is only plain text, so set the HTTP header accordingly
 
 $names = array( // this is our array of names - feel free to change/add as required!
-'borna',
-'brian',
-'algorithm',
-'git'
+  'borna',
+  'brian',
+  'algorithm',
+  'git'
 );
 
 if (in_array($_GET['name'], $names)) { // if that name is in our array above
-
-	//echo $_GET['name'];//'1'; // echo 1 (tells JavaScript we know this person)
-	echo '1';
+  //echo $_GET['name'];//'1'; // echo 1 (tells JavaScript we know this person)
+  echo '1';
+} else { // otherwise
+  //echo $_GET['name'];//echo '0'; // echo 0
+  echo '0';
 }
-
-else { // otherwise
-
-	//echo $_GET['name'];//echo '0'; // echo 0
-	echo '0';
-}
-
-
 ?>
