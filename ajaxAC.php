348fb5760e470b1c7ba112334f341661b0689c0d
shaohuang <brian@dell4600.localdomain>
2010-07-11 01:21:13 -0700

    using an XML file as a response text

--- ajaxAC.php
+++ ajaxAC.php
@@ -1,22 +1,14 @@
 <?php
 
-session_start(); // start a session
-
+//session_start(); // start a session
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
 header("Content-type: text/plain");
-// this is only plain text, so set the HTTP header accordingly
 
-$names = array( // this is our array of names - feel free to change/add as required!
-  'borna',
-  'ghavam',
-  'algorithm',
-  'git'
-);
+$word = $_GET['name'];
 
-if (in_array($_GET['name'], $names)) { // if that name is in our array above
-  //echo $_GET['name'];//'1'; // echo 1 (tells JavaScript we know this person)
-  echo '1';
-} else { // otherwise
-  //echo $_GET['name'];//echo '0'; // echo 0
-  //echo '0';
+$xmldoc = new DOMDocument();
+if (TRUE == $xmldoc->load("words.xml")) {
+  echo $xmldoc->saveXML();
 }
 ?>
